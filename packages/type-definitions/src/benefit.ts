47801c48d4681f18f5d60159cd506f79a8430e92
Yashiro <1161569428@qq.com>
2021-07-16 14:06:38 +0800

    Fix lint

--- packages/type-definitions/src/benefit.ts
+++ packages/type-definitions/src/benefit.ts
@@ -5,7 +5,7 @@ export default {
       total_fee_reduction_quota: 'Compact<Balance>',
       total_market_active_funds: 'Compact<Balance>',
       used_fee_reduction_quota: 'Compact<Balance>',
-      active_era: 'Compact<EraIndex>'
+      active_era: 'Compact<EraIndex>',
     },
     FeeReductionBenefit: {
       funds: 'Balance',
@@ -15,28 +15,25 @@ export default {
       refreshed_at: 'EraIndex',
     },
     FundsType: {
-      _enum: [
-        'SWORK',
-        'MARKET'
-      ]
+      _enum: ['SWORK', 'MARKET'],
     },
     FundsUnlockChunk: {
       value: 'Compact<Balance>',
-      era: 'Compact<EraIndex>'
+      era: 'Compact<EraIndex>',
     },
     MarketBenefit: {
       /// It's total funds value
-      total_funds: `Compact<Balance>`,
+      total_funds: 'Compact<Balance>',
       /// It's own active funds value
-      active_funds: `Compact<Balance>`,
+      active_funds: 'Compact<Balance>',
       /// The used reduction for fee
       used_fee_reduction_quota: 'Compact<Balance>',
       /// The file reward for market
       file_reward: 'Compact<Balance>',
       /// The latest refreshed active era index
-      refreshed_at: `Compact<EraIndex>`,
+      refreshed_at: 'Compact<EraIndex>',
       /// Any balance that is becoming free
-      unlocking_funds: `Vec<FundsUnlockChunk<Balance>>`,
+      unlocking_funds: 'Vec<FundsUnlockChunk<Balance>>',
     },
     SworkBenefit: {
       /// It's total funds value
@@ -44,13 +41,13 @@ export default {
       /// It's own active funds value
       active_funds: 'Compact<Balance>',
       /// The total reduction count for report works
-      total_fee_reduction_count: `u32`,
+      total_fee_reduction_count: 'u32',
       /// The used reduction count for report works
-      used_fee_reduction_count: `u32`,
+      used_fee_reduction_count: 'u32',
       /// The latest refreshed active era index
-      refreshed_at: `Compact<EraIndex>`,
+      refreshed_at: 'Compact<EraIndex>',
       /// Any balance that is becoming free
-      unlocking_funds: `Vec<FundsUnlockChunk<Balance>>`
-    }
+      unlocking_funds: 'Vec<FundsUnlockChunk<Balance>>',
+    },
   },
 };
