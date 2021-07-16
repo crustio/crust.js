1651e6776ebd6f000db9ed162e6d3e2f87d5fff3
Yashiro <1161569428@qq.com>
2021-07-16 13:56:02 +0800

    Add benefit types

--- packages/type-definitions/src/benefit.ts
+++ packages/type-definitions/src/benefit.ts
@@ -2,10 +2,10 @@ export default {
   rpc: {},
   types: {
     EraBenefits: {
-      total_benefits: 'Balance',
-      total_funds: 'Balance',
-      used_benefits: 'Balance',
-      active_era: 'EraIndex',
+      total_fee_reduction_quota: 'Compact<Balance>',
+      total_market_active_funds: 'Compact<Balance>',
+      used_fee_reduction_quota: 'Compact<Balance>',
+      active_era: 'Compact<EraIndex>'
     },
     FeeReductionBenefit: {
       funds: 'Balance',
@@ -14,5 +14,43 @@ export default {
       used_fee_reduction_count: 'u32',
       refreshed_at: 'EraIndex',
     },
+    FundsType: {
+      _enum: [
+        'SWORK',
+        'MARKET'
+      ]
+    },
+    FundsUnlockChunk: {
+      value: 'Compact<Balance>',
+      era: 'Compact<EraIndex>'
+    },
+    MarketBenefit: {
+      /// It's total funds value
+      total_funds: `Compact<Balance>`,
+      /// It's own active funds value
+      active_funds: `Compact<Balance>`,
+      /// The used reduction for fee
+      used_fee_reduction_quota: 'Compact<Balance>',
+      /// The file reward for market
+      file_reward: 'Compact<Balance>',
+      /// The latest refreshed active era index
+      refreshed_at: `Compact<EraIndex>`,
+      /// Any balance that is becoming free
+      unlocking_funds: `Vec<FundsUnlockChunk<Balance>>`,
+    },
+    SworkBenefit: {
+      /// It's total funds value
+      total_funds: 'Compact<Balance>',
+      /// It's own active funds value
+      active_funds: 'Compact<Balance>',
+      /// The total reduction count for report works
+      total_fee_reduction_count: `u32`,
+      /// The used reduction count for report works
+      used_fee_reduction_count: `u32`,
+      /// The latest refreshed active era index
+      refreshed_at: `Compact<EraIndex>`,
+      /// Any balance that is becoming free
+      unlocking_funds: `Vec<FundsUnlockChunk<Balance>>`
+    }
   },
 };
