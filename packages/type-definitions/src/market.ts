7449cb968e96aed4d07476b5bdd0f9c16caa30b3
Yashiro <1161569428@qq.com>
2021-07-22 13:58:45 +0800

    Fix lint

--- packages/type-definitions/src/market.ts
+++ packages/type-definitions/src/market.ts
@@ -2,14 +2,14 @@ export default {
   rpc: {},
   types: {
     FileInfo: {
-      file_size: "u64",
-      spower: "u64",
-      expired_at: "BlockNumber",
-      calculated_at: "BlockNumber",
-      amount: "Balance",
-      prepaid: "Balance",
-      reported_replica_count: "u32",
-      replicas: "Vec<Replica<AccountId>>"
+      file_size: 'u64',
+      spower: 'u64',
+      expired_at: 'BlockNumber',
+      calculated_at: 'BlockNumber',
+      amount: 'Balance',
+      prepaid: 'Balance',
+      reported_replica_count: 'u32',
+      replicas: 'Vec<Replica<AccountId>>',
     },
     Releases: {
       _enum: ['V1_0_0', 'V2_0_0'],
@@ -19,7 +19,7 @@ export default {
       valid_at: 'BlockNumber',
       anchor: 'SworkerAnchor',
       is_reported: 'bool',
-      created_at: "Option<BlockNumber>"
+      created_at: 'Option<BlockNumber>',
     },
     Status: {
       _enum: ['Free', 'Reserved'],
