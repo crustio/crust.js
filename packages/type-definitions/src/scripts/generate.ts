2195d2a8afd40e4a4ee7eabb59082d711858a31a
Yashiro <1161569428@qq.com>
2021-08-06 11:08:32 +0800

    Fix types

--- packages/type-definitions/src/scripts/generate.ts
+++ packages/type-definitions/src/scripts/generate.ts
@@ -1,11 +1,11 @@
-import {mainnet, maxwell} from '../index';
+import {mainnetTypes, maxwellTypes} from '../index';
 import fs from 'fs';
 
 fs.writeFileSync(
   './src/json/mainnetTypes.json',
-  JSON.stringify(mainnet, null, 4)
+  JSON.stringify(mainnetTypes, null, 4)
 );
 fs.writeFileSync(
   './src/json/maxwellTypes.json',
-  JSON.stringify(maxwell, null, 4)
+  JSON.stringify(maxwellTypes, null, 4)
 );
