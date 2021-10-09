ff09f84e0ef38b4308d01fee3a2fd42a9cd7a3e7
ZiBo <ziboilihua@gmail.com>
2021-10-09 17:08:59 +0800

    Support verify elrond signature (#27)

--- packages/ipfs-w3auth/auth-handler/src/authRegistry.ts
+++ packages/ipfs-w3auth/auth-handler/src/authRegistry.ts
@@ -4,6 +4,7 @@ import EthAuth from '@crustio/ipfs-w3auth-ethereum';
 import SolanaAuth from '@crustio/ipfs-w3auth-solana';
 import AvalancheAuth from '@crustio/ipfs-w3auth-avalanche';
 import FlowAuth from '@crustio/ipfs-w3auth-flow';
+import ElrondAuth from '@crustio/ipfs-w3auth-elrond';
 
 const _ = require('lodash');
 
@@ -16,7 +17,8 @@ const authProviders = {
   ...mapBySigType(['ethereum', 'eth', 'polygon', 'pol'], EthAuth),
   ...mapBySigType(['solana', 'sol', 'near', 'nea'], SolanaAuth),
   ...mapBySigType(['avalanche', 'ava'], AvalancheAuth),
-  ...mapBySigType(['flow'], FlowAuth),
+  ...mapBySigType(['flow', 'flo'], FlowAuth),
+  ...mapBySigType(['elrond','elr'], ElrondAuth),
 };
 
 function auth(signatureType: string, data: AuthData): boolean {
