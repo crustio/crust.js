f4549952edee4804312b42ab8e8d3e63db53c1b6
ZiBo <ziboilihua@gmail.com>
2021-10-11 16:39:46 +0800

    Support verify elrond txMsg (#28)
    * Support flow auth
    
    * Code format
    
    * Modify flow desc and add flow dependencies to auth-handler
    
    * Update yarn.lock
    
    * Modify package name
    
    * Support verify elrond signature
    
    * Add short-cut
    
    * Support verify elrond txMsg

--- packages/ipfs-w3auth/auth-elrond/src/index.ts
+++ packages/ipfs-w3auth/auth-elrond/src/index.ts
@@ -1,25 +1,17 @@
 import {AuthData} from './types';
-import {Signature} from '@elrondnetwork/erdjs/out/signature';
-import {SignableMessage} from '@elrondnetwork/erdjs/out/signableMessage';
 import {Address, UserPublicKey, UserVerifier} from '@elrondnetwork/erdjs';
 
-/**
- * SolanaAuth expects BS58 public address, and hex signature string
- */
-function auth(data: AuthData): boolean {
-  const signedM = new SignableMessage({
-    address: new Address(data.address),
-    message: Buffer.from(data.address),
-    signature: new Signature(data.signature),
-  });
-  const publicKey = new UserPublicKey(
-    Address.fromString(data.address).pubkey()
-  );
-  const verifier = new UserVerifier(publicKey);
-  const valid = verifier.verify(signedM);
-  return valid;
+async function auth(data: AuthData): Promise<boolean> {
+    const publicKey = new UserPublicKey(
+        Address.fromString(data.address).pubkey()
+    );
+    const valid = publicKey.verify(
+        Buffer.from(data.txMsg, 'hex'),
+        Buffer.from(data.signature, 'hex')
+    );
+    return valid;
 }
 
 export default {
-  auth,
+    auth,
 };
