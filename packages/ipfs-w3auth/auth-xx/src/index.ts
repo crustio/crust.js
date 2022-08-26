db9f25c4ff83c480b2cbcbfd8e46593dfff1cf59
LowEntropyBody <jszyyx@163.com>
2022-08-26 16:29:28 +0800

    use evn

--- packages/ipfs-w3auth/auth-xx/src/index.ts
+++ packages/ipfs-w3auth/auth-xx/src/index.ts
@@ -4,7 +4,12 @@ import axios from 'axios'
 
 async function auth(data: AuthData): Promise<boolean> {
   try {
-    const res = await axios.post('http://localhost:38297/verify', {
+    var host = "http://localhost:38297"
+    if (process.env.XX_VALIDATE_HOST) {
+      host = process.env.XX_VALIDATE_HOST
+    }
+
+    const res = await axios.post(`${host}/verify`, {
       pubKey: data.address,
       signature: data.signature,
       userName: data.txMsg,
